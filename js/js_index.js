490569f277fa630e2d9b2629eb015fc02bc9956b
Nereklamiok <kosarev_kirilll@mail.ru>
2017-11-21 19:58:11 +0300

    модалка Login готова

--- js/js_index.js
+++ js/js_index.js
@@ -1,6 +1,12 @@
 var maps = document.querySelector('.maps');
 var modal_map = document.querySelector('.modal_map');
 var cross = document.querySelector('.cross');
+var modal_login = document.querySelector('.modal_login');
+var entrance = document.querySelector('.entrance');
+entrance.addEventListener('click',function(event) {
+    event.preventDefault();
+    modal_login.classList.toggle('login_open');
+});
 maps.addEventListener('click',function(event) {
     event.preventDefault();
     modal_map.classList.toggle('map_open');
