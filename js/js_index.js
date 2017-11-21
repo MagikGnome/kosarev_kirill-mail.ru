37818a118226db04a6bb5cc761fb88d1a29ec4d0
Nereklamiok <kosarev_kirilll@mail.ru>
2017-11-21 21:18:07 +0300

    Кресты завезли

--- js/js_index.js
+++ js/js_index.js
@@ -1,8 +1,9 @@
 var maps = document.querySelector('.maps');
 var modal_map = document.querySelector('.modal_map');
-var cross = document.querySelector('.cross');
+var cross_map = document.querySelector('.cross_map');
 var modal_login = document.querySelector('.modal_login');
 var entrance = document.querySelector('.entrance');
+var cross_login = document.querySelector('.cross_login');
 entrance.addEventListener('click',function(event) {
     event.preventDefault();
     modal_login.classList.toggle('login_open');
@@ -11,7 +12,11 @@ maps.addEventListener('click',function(event) {
     event.preventDefault();
     modal_map.classList.toggle('map_open');
 });
-cross.addEventListener('click',function(event) {
+cross_map.addEventListener('click',function(event) {
     event.preventDefault();
     modal_map.classList.toggle('map_open');
+});
+cross_login.addEventListener('click',function(event) {
+    event.preventDefault();
+    modal_login.classList.toggle('login_open');
 });
